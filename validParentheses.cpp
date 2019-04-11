c0e4821b0aae57d89937bd2af2a785eee631106c
Cody Uhi <codylkuhi@gmail.com>
2019-04-11 15:08:13 -0600

    Add files via upload
    Implemented stack-based solution, which instead of calling recursive function with string parameter (finite char count), use stack which can hold many more chars.  Also faster.

--- validParentheses.cpp
+++ validParentheses.cpp
@@ -1,131 +1,37 @@
 class Solution {
 public:
-    bool processReg(string s, int & count){
-        //cout << "Entered processReg" << endl;
-        switch(s[0]){
-            case ')':
-                //cout << "Processed )" << endl;
-                return true;
-                break;
-            case '(':
-                s = s.substr(1,s.size()-1);
-                count++;
-                if(!processReg(s,count)){
-                    return false;
-                }
-                break;
-            case '[':
-                s = s.substr(1,s.size()-1);
-                count++;
-                if(!processSqr(s,count)){
-                    return false;
-                }
-                break;
-            case '{':
-                s = s.substr(1,s.size()-1);
-                count++;
-                if(!processCurl(s,count)){
-                    return false;
-                }
-                break;
-            default:
-                return false;
-                break;
-        }
-        return true;
-    }
-    bool processSqr(string s, int & count){
-        //cout << "Entered processSqr" << endl;
-        switch(s[0]){
-            case ']':
-                return true;
-                break;
-            case '(':
-                s = s.substr(1,s.size()-1);
-                if(!processReg(s,count)){
-                    return false;
-                }
-                break;
-            case '[':
-                s = s.substr(1,s.size()-1);
-                if(!processSqr(s,count)){
-                    return false;
-                }
-                break;
-            case '{':
-                s = s.substr(1,s.size()-1);
-                if(!processCurl(s,count)){
-                    return false;
-                }
-                break;
-            default:
-                return false;
-                break;
-        }
-        return true;
-    }
-    bool processCurl(string s, int & count){
-        //cout << "Entered processCurl" << endl;
-        switch(s[0]){
-            case '}':
-                return true;
-                break;
-            case '(':
-                s = s.substr(1,s.size()-1);
-                if(!processReg(s,count)){
-                    return false;
-                }
-                break;
-            case '[':
-                s = s.substr(1,s.size()-1);
-                if(!processSqr(s,count)){
-                    return false;
-                }
-                break;
-            case '{':
-                s = s.substr(1,s.size()-1);
-                if(!processCurl(s,count)){
-                    return false;
-                }
-                break;
-            default:
-                return false;
-                break;
-            }        
-        return true;
-    }
-    
     bool isValid(string s) {
-        while(s != ""){
-            int count = 0;
-            if(s.length() > 0){
-                switch(s[0]){
-                    case '(':
-                        s = s.substr(1,s.size()-1);
-                        count++;
-                        if(!processReg(s,count)){
-                            //cout << "GOT HERE 0" << endl;
+        stack<char> stack;
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
+                stack.push(s[i]);
+            }else if(stack.size() > 0){
+                switch(s[i]){
+                    case ')':
+                        if(stack.top() != '('){
                             return false;
                         }
+                        stack.pop();
                         break;
-                    case '[':
-                        s = s.substr(1,s.size()-1);
-                        count++;
-                        if(!processSqr(s,count)){
+                    case ']':
+                        if(stack.top() != '['){
                             return false;
                         }
+                        stack.pop();
                         break;
-                    case '{':
-                        s = s.substr(1,s.size()-1);
-                        count++;
-                        if(!processCurl(s,count)){
+                    case '}':
+                        if(stack.top() != '{'){
                             return false;
                         }
+                        stack.pop();
                         break;
                 }
-                //cout << "s is " << s << endl;
+            }else{
+                return false;
             }
-            s = s.substr(count,s.size()-1);
+        }
+        if(stack.size() > 0){
+            return false;
         }
         return true;
     }
