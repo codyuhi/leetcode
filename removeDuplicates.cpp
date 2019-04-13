5fb9bc8cb741976cf09d0db57d71a6898db14485
Cody Uhi <codylkuhi@gmail.com>
2019-04-12 18:40:57 -0600

    Add files via upload

--- removeDuplicates.cpp
+++ removeDuplicates.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        for(int i = 0; i < nums.size(); i++){
-            if(i+1 < nums.size()){
-                if(nums[i] == nums[i+1]){
-                    nums.erase(nums.begin() + i);
-                    i--;
-                }
+        if(empty(nums))
+            return 0;
+        int w = 1;
+        for(int i = 1;i<nums.size();i++){
+            if(nums[w-1]!=nums[i]){
+                nums[w++] = nums[i];
             }
         }
-        return nums.size();
+        return w;
     }
 };
