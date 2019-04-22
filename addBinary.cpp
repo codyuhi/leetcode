d26cd628c70dd186da2b4aecadc67dfb8b92d7af
Cody Uhi <codylkuhi@gmail.com>
2019-04-22 17:01:24 -0600

    Add files via upload

--- addBinary.cpp
+++ addBinary.cpp
@@ -5,34 +5,65 @@ public:
         reverse(a.begin(), a.end());
         reverse(b.begin(), b.end());
         if(a.length() < b.length()){
-            for(int i = 0; i < a.length(); i++){
-                if(a[i] == '1'){
-                    if(b[i] == '1'){
-
-                    }else{
-                        b[i] = '1';
-                    }
-                }
-            }
-            reverse(b.begin(), b.end());
-            return b;
-        }else{
-            for(int i = 0; i < b.length(); i++){
-                if(b[i] == '1'){
-                    if(a[i] == '1'){
-                        if(i >= b.length()-1){
-                            a[i] = '0';
-                            a += '1';
-                        }else{
-                            
-                        }
-                    }else{
-                        a[i] = '1';
-                    }
-                }
-            }
-            reverse(a.begin(), a.end());
-            return a;
+            string temp = a;
+            a = b;
+            b = temp;
         }
+        bool carry = false;
+        for(int i = 0; i < b.length(); i++){
+            if(carry){
+                if(b[i] == '1' || a[i] == '1'){
+                    if(b[i] != '1' || a[i] != '1'){
+                        a[i] = '0';
+                    }
+                }else{
+                    a[i] = '1';
+                    carry = false;
+                }
+            }else if(b[i] == '1' && a[i] == '1'){
+                carry = true;
+                a[i] = '0';
+            }else if(b[i] == '1' || a[i] == '1'){
+                a[i] = '1';
+            }
+        }
+        if(carry){
+            for(int i = b.length(); i < a.length(); i++){
+                if(a[i] == '1'){
+                    a[i] = '0';
+                }else{
+                    a[i] = '1';
+                    carry = false;
+                    break;
+                }
+            }
+            if(carry){
+                a += '1';
+            }
+        }
+        reverse(a.begin(), a.end());
+        return a;
     }
 };
+
+/*//this is another solution:
+class Solution
+{
+public:
+    string addBinary(string a, string b)
+    {
+        string s = "";
+        
+        int c = 0, i = a.size() - 1, j = b.size() - 1;
+        while(i >= 0 || j >= 0 || c == 1)
+        {
+            c += i >= 0 ? a[i --] - '0' : 0;
+            c += j >= 0 ? b[j --] - '0' : 0;
+            s.insert(0,1,char(c % 2 + '0'));
+            c /= 2;
+        }
+        
+        return s;
+    }
+};
+*/
