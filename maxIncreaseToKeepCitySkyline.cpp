9d243649fde271ef8f81742aa207fa77eab1234b
codyuhi <codylkuhi@gmail.com>
2019-06-27 23:26:35 -0400

    Update maxIncreaseToKeepCitySkyline.cpp

--- maxIncreaseToKeepCitySkyline.cpp
+++ maxIncreaseToKeepCitySkyline.cpp
@@ -7,7 +7,9 @@ public:
         for (int i = 0; i < grid.size(); i++)
         {
             int x = 0;
+            int y = 0;
             int topX = 0;
+            int topY = 0;
             for (int j = 0; j < grid[i].size(); j++)
             {
                 x = grid[i][j];
@@ -15,24 +17,16 @@ public:
                 {
                     topX = x;
                 }
-            }
-            //cout << topX << endl;
-            //if(i == 0){
-                xMax.push_back(topX);
-                cout << "Pushed back " << topX << endl;
-            //}
-        }
-        for(int i = 0; i < grid.size(); i++){
-            int y = 0;
-            int topY = 0;
-            for(int j = 0; j < grid.size(); j++){
                 y = grid[j][i];
-                if(y > topY){
+                if (y > topY)
+                {
                     topY = y;
                 }
             }
-            yMax.push_back(topY);
-            cout << "pushed back " << topY << endl;
+                xMax.push_back(topX);
+                cout << "Pushed back X " << topX << endl;
+                yMax.push_back(topY);
+                cout << "Pushed back Y " << topY << endl;
         }
         return 0;
     }
