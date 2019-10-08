76733271704357180740435e06e54c512220e44b
Cody <codylkuhi@gmail.com>
2019-10-07 18:01:11 -0600

    Update validateStackSequences.cpp
    Runtime: 8 ms, faster than 92.20% of C++ online submissions for Validate Stack Sequences.
    Memory Usage: 9 MB, less than 100.00% of C++ online submissions for Validate Stack Sequences.

--- validateStackSequences.cpp
+++ validateStackSequences.cpp
@@ -4,34 +4,22 @@ public:
         if(pushed.size() != popped.size()){
             return false;
         }
-        if(pushed.size() == 0){
-            return true;
-        }
         stack<int> myStack;
-        myStack.push(pushed[0]);
-        int pushedValue = 1;
         int poppedValue = 0;
-        while(pushedValue != pushed.size() || poppedValue != pushed.size()){
-            if(myStack.size() > 0){
-                if(myStack.top() == popped[poppedValue]){
-                    myStack.pop();
-                    poppedValue++;
-                }else if(pushedValue < pushed.size()){
-                    myStack.push(pushed[pushedValue]);
-                    pushedValue++;
-                }else{
-                    return false;
-                }
-            }else{
-                if(pushedValue < pushed.size()){
-                    myStack.push(pushed[pushedValue]);
-                    pushedValue++;
-                }else{
-                    return false;
+        for(int i = 0; i < pushed.size(); i++){
+            myStack.push(pushed[i]);
+            while(poppedValue < popped.size() && myStack.top() == popped[poppedValue]){
+                myStack.pop();
+                poppedValue++;
+                if(myStack.size() == 0){
+                    break;
                 }
             }
-
         }
-        return true;
+        if(myStack.size() > 0){
+            return false;
+        }else{
+            return true;
+        }
     }
 };
