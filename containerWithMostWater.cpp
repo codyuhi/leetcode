734a78be5d5d03b236de18944a743d6fa05fd8fd
Cody Uhi <codylkuhi@gmail.com>
2019-12-19 13:19:49 -0700

    Update containerWithMostWater.cpp

--- containerWithMostWater.cpp
+++ containerWithMostWater.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int max = 0;
-        for(int i = 0; i < height.size(); i++){
-            for(int j = 0; j < height.size(); j++){
-                if( i != j ){
-                    if ( height[i] < height[j] ){
-                        int tempMax = height[i] * abs(j - i);
-                        if( tempMax > max ){
-                            max = tempMax;
-                        }
-                    } else {
-                        int tempMax = height[j] * abs(i - j);
-                        if( tempMax > max ){
-                            max = tempMax;
-                        }
-                    }
-                }
-            }
+        int maximum = 0;
+        
+        //two pointers solution
+        int first = 0;
+        int last = height.size() - 1;
+        
+        while (first < last) {
+            //calculate area
+            maximum = max( (min(height[first], height[last]) * (last - first)), maximum) ;        
+            
+            //move pointers
+            if (height[first] < height[last]) first++;
+            else last--;
         }
-        return max;
+        
+        return maximum;
     }
-};
+}; 
